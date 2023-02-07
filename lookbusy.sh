f5ca8fd528b70dd0cde81578833266d1f4dd652d
chacha20 <46554415+chacha20@users.noreply.github.com>
2023-02-07 15:45:54 +0800

    Update lookbusy.sh

--- lookbusy.sh
+++ lookbusy.sh
@@ -12,8 +12,9 @@ touch $basedir/openssl.log
 
 for i in {1..10}
 do
-    echo `date`>>$basedir/openssl.log
+    echo 'ROUND:'>>$basedir/openssl.log
     echo $newline >> $basedir/openssl.log
+    echo 'ROUND:'
     openssl speed -evp aes-128-gcm -seconds 3 >> $basedir/openssl.log;
     sleep 1
 done
@@ -24,7 +25,9 @@ rm -rf $basedir/openwrt*
 
 for j in {1..50}
 do
-    wget https://github.com/coolsnowwolf/lede/releases/download/20220716/openwrt-rockchip-R22.10.10-Docker-hinlink_opc-h68k-d-squashfs-sysupgrade.img.gz -o $basedir/openwrt.tmg.gz >> $basedir/wget.log
+    echo 'ROUND:'
+    wget -v --show-progress https://github.com/coolsnowwolf/lede/releases/download/20220716/openwrt-rockchip-R22.10.10-Docker-hinlink_opc-h68k-d-squashfs-sysupgrade.img.gz -O $basedir/openwrt.tmg.gz >> $basedir/wget.log
     rm -rf $basedir/openwrt.tmg.gz*;
+    rm -rf 
     sleep 1;
 done
