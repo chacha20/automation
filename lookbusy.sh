d1231b96d4d5cbdd0275dfcb048cc5257aab4c5a
chacha20 <46554415+chacha20@users.noreply.github.com>
2023-02-07 17:54:13 +0800

    Update lookbusy.sh

--- lookbusy.sh
+++ lookbusy.sh
@@ -15,12 +15,12 @@ done
 
 rm -rf $basedir/lookbusy.log
 
-# filesize 100M
+# filesize 100M/limits to 4MB/s
 for j in {1..50}
 do
     echo `date`>>$basedir/lookbusy.log
-    wget https://github.com/coolsnowwolf/lede/releases/download/20220716/openwrt-rockchip-R22.10.10-Docker-hinlink_opc-h68k-d-squashfs-sysupgrade.img.gz -O $basedir/openwrt.tmg.gz
-    rm -rf $basedir/openwrt.tmg.gz*;
+    wget --limit-rage=4M https://github.com/coolsnowwolf/lede/releases/download/20220716/openwrt-rockchip-R22.10.10-Docker-hinlink_opc-h68k-d-squashfs-sysupgrade.img.gz -O $basedir/openwrt.tmg.gz
+    rm -rf $basedir/openwrt*.img.gz*;
     rm -rf 
     sleep 1;
 done
