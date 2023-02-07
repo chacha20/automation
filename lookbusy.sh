ca8a19bf270ad13d01e4293714395420cfbb4c58
chacha20 <46554415+chacha20@users.noreply.github.com>
2023-02-07 15:19:44 +0800

    Update lookbusy.sh

--- lookbusy.sh
+++ lookbusy.sh
@@ -1,32 +1,31 @@
 #!/bin/bash
-
-cd /tmp
-if test ! -e logs;
-then mkdir /tmp/logs;
+basedir=/tmp/logs
+if test ! -e $basedir;
+then mkdir $basedir;
 fi
 
 # 清除上一轮日志
-rm -rf /tmp/logs/openssl*.log
+rm -rf $basedir/openssl*.log
 # 循环执行10次
 for i in {1..10}
 do
     # 每次计算150秒，openssl内部执行六轮，共150秒*6=900秒
-    openssl speed -evp aes-128-gcm -seconds 3 >> /tmp/logs/openssl${i}.log;
+    openssl speed -evp aes-128-gcm -seconds 3 >> $basedir/openssl${i}.log;
     # 休息10秒，避免cpu监控连续CPU超长波峰
     sleep 1
 done
 
 # 清除wget日志
-rm -rf /tmp/logs/wget*.log
+rm -rf $basedir/wget*.log
 # 循环执行下载50次
-for i in {1..50}
+for j in {1..50}
 do
     # 删除前期下载文件
-    rm -rf /tmp/logs/openwrt.tmg.gz*;
+    rm -rf $basedir/openwrt.tmg.gz*;
     # 执行一次下载，约100M
-    wget https://github.com/coolsnowwolf/lede/releases/download/20220716/openwrt-rockchip-R22.10.10-Docker-hinlink_opc-h68k-d-squashfs-sysupgrade.img.gz -o /tmp/logs/openwrt.tmg.gz >> ~/logs/wget${i}.log
+    wget https://github.com/coolsnowwolf/lede/releases/download/20220716/openwrt-rockchip-R22.10.10-Docker-hinlink_opc-h68k-d-squashfs-sysupgrade.img.gz -o $basedir/openwrt.tmg.gz >> $basedir/wget${i}.log
     # 删除下载文件
-    rm -rf /tmp/logs/openwrt.tmg.gz*;
+    rm -rf $basedir/openwrt.tmg.gz*;
     # 休息30秒，避免被ban
     sleep 1;
 done
